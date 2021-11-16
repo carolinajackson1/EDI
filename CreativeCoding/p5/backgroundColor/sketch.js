38288be2feb2d7dc73e89c6fb95c58b7196f79ac
carolinajackson1 <carolina.jackson@gmail.com>
2021-11-16 14:08:23 -0500

    program to make colorful background

---
+++ CreativeCoding/p5/backgroundColor/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+  createCanvas (windowwidth, windowheight);// this creates the window width and height to be the size of my browser
+background (255,0,0);// red background
+}
+
+function draw() {
+
+}
